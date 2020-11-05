a8d6735aa7e3c9f2c70bef15e17c9ea676c6395b
Dennis Peters <denjpeters@gmail.com>
2020-11-05 14:09:04 -0500

    Comments on the IW Server Data object

--- src/WebControls/IWServerData.tsx
+++ src/WebControls/IWServerData.tsx
@@ -11,66 +11,122 @@ import _ from 'lodash'
  * See the accompanying IWServerData.md documentation for examples how to use this control.
  */
 
+/**
+ * The standard structure of the response object, which can be a generic type, undefined or null.
+ *
+ * Default the value to undefined to automatically trigger the API.
+ */
 export type TServerData<T = any> = T | undefined | null
 
-export interface IServerDataUpdatedState<U = any> {
+/**
+ * An interface that allows for a script-driven API to occur.
+ *
+ * Implement the following in the DOM structure:
+ * <ServerData {...serverDataUpdateProps} setUpdateResponse={setServerDataUpdateProps} />
+ */
+export interface IServerDataUpdatedState<R = any, U = any> {
+	/** First item of URL */
 	item?: string
+	/** Second item of URL */
 	updateVerb: string
-	updateRequest: any
+	/** Request package sent in the body of the POST */
+	updateRequest: R
+	/** Message to display to the user after a successful API call */
 	updateMessage?: string
+	/** Sets the state of the response object to null a successful API call */
 	setUpdateResponse?: Dispatch<SetStateAction<U | null>>
+	/** Action fired with the response object on completion */
 	updatedAction?: (response: U) => void
+	/** Fired when the API starts */
 	startingAction?: () => void
+	/** Fired if the API fails */
 	catchAction?: (err: AxiosError) => void
+	/** Fired at end of API, either successful or unsuccessful */
 	finallyAction?: () => void
+	/** Fired if the API fails */
 	failedAction?: (serverStatus: any) => void
-	globalActivityOverlay?: boolean
+	/** Tells the control to display the local child activity overlay while processing */
 	noActivityOverlay?: boolean
+	/** Tells the control to display the global activity overlay while processing */
+	globalActivityOverlay?: boolean
 }
 
-export type TServerDataUpdatedState<U = any> = IServerDataUpdatedState<U> | null
+export type TServerDataUpdatedState<R = any, U = any> = IServerDataUpdatedState<R, U> | null
 
 // G = Get, U = Update
-export interface IIWQueryProps<G = any, U = any> {
+export interface IIWQueryProps<GR = any, G = any, UR = any, U = any> {
+	/** Tells the control to display the local child activity overlay while processing */
 	noActivityOverlay?: boolean
+	/** Tells the control to display the global activity overlay while processing */
 	globalActivityOverlay?: boolean
-
+	
+	/** First item of URL */
 	item?: string
+	/** Second item of URL */
 	verb?: string
-	request?: any
+	/** Request package sent in the body of the POST.  If changed, will re-fire the API if this changes and noRefreshOnRequestChange is not true. */
+	request?: GR
+	/** The response object shared with the control.  Set to 'undefined' for the API to initiate. */
 	response?: TServerData<G>
+	/** Sets the state of the response object to null (if failed), or the server data */
 	setResponse?: Dispatch<SetStateAction<G | null>>
+	/** Message to display to the user after a successful API call */
 	responseMessage?: string
+	/** Ignores changes the request object, that would otherwise re-fire the API. */
 	noRefreshOnRequestChange?: boolean
+	/** If this value changes, the API will re-fire. */
 	forceRefresh?: any
+	/** Function called when the API determines it needs to fire */
 	startingAction?: () => void
+	/** Called if the API fails */
 	failedAction?: (serverStatus: any) => void
+	/** Called after the API returns, whether successful or failed. */
 	finallyAction?: () => void
 
+	/** Second item of URL */
 	updateVerb?: string
-	updateRequest?: any
+	/** Request package sent in the body of the POST */
+	updateRequest?: UR
+	/** Sets the state of the response object to null a successful API call */
 	setUpdateResponse?: Dispatch<SetStateAction<U | null>>
+	/** Message to display to the user after a successful API call */
 	updateMessage?: string
+	/** After the response is received from the server, this method is fired if successful. */
 	updatedAction?: (response: U) => void
 
+	/** Items to be shown in side the control */
 	children?: false | ReactNodeArray | ReactNode
+	/** Items to be shown when the API is working.  Defaults to the <ActivityOverlayControl/> */
 	loadingReactNodes?: ReactNodeArray | ReactNode
+	/** Items to be shown in the API fails (e.g. when the response is null) */
 	failedReactNodes?: ReactNodeArray | ReactNode
 	
+	/** Prefix that the control will append an item and verb to */
 	urlPrefix?: string
+	/** An object that will be passed to the server in the header */
 	authorizationHeader?: any
 	
+	/** The actual error from Axios on failure */
 	axiosResponseAction?: (axiosResponse: AxiosResponse) => void
+	/** A function that is fired every time an API happens with the header data in it */
 	handleServerData?: (serverData: any) => boolean
+	/** A function that is called from the control whenever it needs to alert the user of something */
 	showUserMessage?: (message: string, failed?: boolean) => void
+	/** A function called when the API fails */
 	catchAction?: (err: AxiosError) => void
 
+	/** Shows console logs on every fire */
 	verboseConsole?: boolean
+	/** Shows more console logs on every fire */
 	superVerboseConsole?: boolean
+	/** Turns off Axios.withCredentials */
 	noCredentials?: boolean
 }
 
-export const IWServerData = <G, U>(props: IIWQueryProps<G, U>) => {
+/**
+ * The IWServerData control is a React control that calls API's to a server and manages the state of the data in its control.
+ */
+export const IWServerData = <GR, G, UR, U>(props: IIWQueryProps<GR, G, UR, U>) => {
 	const isMounted = useRef(true)
 	const forceRefreshRef = useRef(props.forceRefresh)
 	const lastRequest = useRef(props.request)
