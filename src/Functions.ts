473779208cf4fe67c07de4c01669c8a7c65e2a6c
Dennis Peters <denjpeters@gmail.com>
2021-04-08 00:02:01 -0400

    Better copy

--- src/Functions.ts
+++ src/Functions.ts
@@ -40,32 +40,32 @@ export const ClassNames = (classes: {[key: string]: boolean}): string => {
 
 export const HasPathComponent = (search: string): boolean => {
 	let searchCalc = search.toLowerCase()
-	
+
 	if (!searchCalc.startsWith('/')) {
 		searchCalc = '/' + searchCalc
 	}
-	
+
 	if (!searchCalc.endsWith('/')) {
 		searchCalc += '/'
 	}
-	
+
 	let pathName = window.location.pathname.toLowerCase()
 	if (!pathName.endsWith('/')) {
 		pathName += '/'
 	}
-	
+
 	return pathName.indexOf(searchCalc) >= 0
 }
 
 export const GetPathComponentAfter = (search: string): any | undefined => {
 	let searchCalc = search.toLowerCase()
-	
+
 	if (!searchCalc.endsWith('/')) {
 		searchCalc += '/'
 	}
-	
+
 	const startPos = window.location.pathname.toLowerCase().indexOf(searchCalc)
-	
+
 	if (startPos >= 0) {
 		const after = window.location.pathname.substr(startPos + searchCalc.length)
 		const slashPos = after.toLowerCase().indexOf('/')
@@ -80,23 +80,23 @@ export const GetPathComponentAfter = (search: string): any | undefined => {
 
 export const GetPathThrough = (search: string): any | undefined => {
 	let searchCalc = search.toLowerCase()
-	
+
 	const startPosSlash = window.location.pathname.toLowerCase().lastIndexOf(searchCalc + '/')
-	
+
 	if (startPosSlash >= 0) {
 		return window.location.pathname.substr(0, startPosSlash + searchCalc.length)
 	}
-	
+
 	const startPosNoSlash = window.location.pathname.toLowerCase().lastIndexOf(searchCalc)
-	
+
 	if (startPosNoSlash >= 0) {
 		const possibleComplete = window.location.pathname.substr(0, startPosNoSlash + searchCalc.length)
-		
+
 		if (possibleComplete.length === window.location.pathname.length) {
 			return possibleComplete
 		}
 	}
-	
+
 	return undefined
 }
 
@@ -121,17 +121,17 @@ export const DownloadBase64Data = (fileName: string, base64: string, type: strin
 	if (!!window.navigator.msSaveBlob) {
 		// IE
 		const byteCharacters = atob(base64.replace(/^[^,]+,/, '').replace(/\r\n/g, ''))
-		
+
 		let byteNumbers = new Array(byteCharacters.length)
-		
+
 		for (let i = 0; i < byteCharacters.length; i++) {
 			byteNumbers[i] = byteCharacters.charCodeAt(i)
 		}
-		
+
 		const byteArray = new Uint8Array(byteNumbers)
-		
+
 		const blob = new Blob([byteArray], {type: type})
-		
+
 		window.navigator.msSaveOrOpenBlob(blob, fileName)
 	} else {
 		const link = document.createElement('a')
@@ -149,19 +149,39 @@ export const CopyRefToClipboard = (ref: any): boolean => {
 		if (sel) {
 			// unselect any element in the page
 			sel.removeAllRanges()
-			
+
+			let ths = ref.current.getElementsByTagName('th') as any[]
+			for (let i = 0; i < ths.length; i++) {
+				ths[i].setAttribute('copyuserselect', ths[i].style.userSelect)
+				ths[i].style.userSelect = 'auto'
+			}
+			let tds = ref.current.getElementsByTagName('td') as any[]
+			for (let i = 0; i < tds.length; i++) {
+				tds[i].setAttribute('copyuserselect', tds[i].style.userSelect)
+				tds[i].style.userSelect = 'auto'
+			}
+
 			try {
-				range.selectNodeContents(ref.current as any)
-				sel.addRange(range)
-			} catch (e) {
 				range.selectNode(ref.current as any)
 				sel.addRange(range)
+			} catch (e) {
+				range.selectNodeContents(ref.current as any)
+				sel.addRange(range)
 			}
-			
+
 			document.execCommand('copy')
-			
+
 			sel.removeAllRanges()
-			
+
+			for (let i = 0; i < ths.length; i++) {
+				ths[i].style.userSelect = ths[i].getAttribute('copyuserselect')
+				ths[i].removeAttribute('copyuserselect')
+			}
+			for (let i = 0; i < tds.length; i++) {
+				tds[i].style.userSelect = tds[i].getAttribute('copyuserselect')
+				tds[i].removeAttribute('copyuserselect')
+			}
+
 			return true
 		}
 	}
